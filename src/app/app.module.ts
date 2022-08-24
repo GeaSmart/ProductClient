1bcf0399c393a3828a64457a1322b6dd49c5a5cc
Gerson Azabache <gerson_dunk@hotmail.com>
2022-08-24 15:46:37 -0500

    componentes, services and routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProductsComponent } from './products/products.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ProductsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
